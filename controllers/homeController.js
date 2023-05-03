59ebcab1425a6fb241586ccf65abe2ebab275a7f
nurulQuamila <nurulizah.elsya@gmail.com>
2023-05-03 19:43:41 +0200

    Sprint 3
    the third sprint include lesson 10-12

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,14 +1,39 @@
-exports.sendReqVendors = (req, res) => {
-    let vend = req.params.url;
-    res.send(`This is the page for ${vend}`);
+exports.showIndex = (req, res) => {
+    res.render("index");
+}
+
+exports.showContact = (req, res) => {
+    res.render("contact");
 };
 
-exports.sendIndex = (req, res) => {
-    res.send("We are here to make your dreams come true");
-}
+// page after user submitted a contact form
+exports.submittedContact = (req, res) => {
+    res.render("thanks");
+};
 
 exports.showData = (req, res) => {
     console.log(req.body);
     console.log(req.query);
     res.send("POST Successful!");
+};
+
+var vendorList = [
+    {
+        title: "Park",
+        cost: 10
+    },
+    {
+        title: "Hotel",
+        cost: 30
+    },
+    {
+        title: "Restaurant",
+        cost: 20
+    }
+];
+
+exports.showVendors = (req, res) => {
+    res.render("vendors", {
+        offeredVendors: vendorList
+    });
 };
--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 const homeController = require("./controllers/homeController");
+const errorController = require("./controllers/errorController");
 
-const port = 3000,
-express = require("express"),
+const express = require("express"),
+layouts = require("express-ejs-layouts"),
 app = express();
 
-app.get("/", homeController.sendIndex);
-app.get("/services/:vendors", homeController.sendReqVendors);
+app.set("port", process.env.PORT || 3000);
+app.set("view engine", "ejs");
+app.use(layouts);
+app.use(express.static("public"));
+
+app.get("/", homeController.showIndex);
+app.get("/vendors", homeController.showVendors);
+app.get("/contact", homeController.showContact);
+app.post("/contact", homeController.submittedContact);
 
 app.use((req, res, next) => {
     console.log(`request made to: ${req.url}`);
@@ -21,6 +29,12 @@ app.use(express.json());
 
 app.post("/", homeController.showData);
 
-app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
+// error handlers as middleware functions
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
+
+app.listen(app.get("port"), () => {
+    console.log(
+        `Server running at http://localhost:${app.get("port")}`
+    );
 });
